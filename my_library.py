17d96935800e5ad303f075b95cef5287dcc7a3e3
MD MASBHA UDDIN <masbhanoman@gmail.com>
2020-12-31 15:14:48 +0600

    Update my_library.py
    added substitute func

--- my_library.py
+++ my_library.py
@@ -1,14 +1,23 @@
+# *args returns  a tupple
 def addition(*args):
     result = 0
     for x in args:
         result+=x
     return result
 
+# enumerate() adds a counter to an iterable and returns it in a form of enumerate object.
+#This enumerate object can then be used directly in for loops or 
+#be converted into a list of tuples using list() method.
 def subtraction(*args):
-    result = 0
-    for x in args:
-        result-=x
-    return result
+    result = list(args)
+    res = result[0]
+    for x, y in enumerate(result):
+        print(x,y)
+        try:
+            res = res - result[x+1]
+        except IndexError:
+            pass
+    return res
 
 def factorial(n):
     if n == 0:        
