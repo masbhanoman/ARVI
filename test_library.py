dc52798050d3bfa7b36f00d24e07dc292fdbcb63
MD MASBHA UDDIN <masbhanoman@gmail.com>
2020-12-31 17:52:18 +0600

    Update test_library.py
    took the function name from user input and added a check in locals() dict

--- test_library.py
+++ test_library.py
@@ -1,10 +1,16 @@
+import code_library
 import inspect
-import my_library
+
+if "code_library" in locals():
+    str = input()
+    obj = ".".join(["code_library", str])
+    
+    try:
+        print(inspect.getsource(eval(obj)))
+        #print(open(my_library.__file__).read())
+    except OSError:
+        print("Source not available, possibly a C module.")
+else:
+    print('you havn"t imported the library  ')
 
 
-
-try:    
-    print (inspect.getsource(my_library.add))
-    #print(open(my_library.__file__).read())
-except OSError:
-    print("Source not available, possibly a C module.")
